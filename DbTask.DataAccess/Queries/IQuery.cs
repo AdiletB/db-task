88681f58c9d4830fb96a1b136af987af9a1299df
AdiletB <a.balabekov@outlook.com>
2024-12-09 19:03:44 +0500

    Add queries

---
+++ DbTask.DataAccess/Queries/IQuery.cs
@@ -0,0 +1,10 @@
+﻿using DbTask.DataAccess.Models;
+using System.Collections.Generic;
+
+namespace DbTask.DataAccess.Queries
+{
+    public interface IQuery<T> where T : Entity
+    {
+        public List<T> Execute();
+    }
+}
---
+++ DbTask.DataAccess/Queries/Tests/GetByBrowser.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using DbTask.DataAccess.Models;
+
+namespace DbTask.DataAccess.Queries.Tests
+{
+    public class GetByBrowser : IQuery<Test>
+    {
+        protected string Browser { get; private set; }
+
+        public GetByBrowser(string browser)
+        {
+            Browser = browser;
+        }
+
+        public List<Test> Execute()
+        {
+            using (var connection = Database.Instance.Connection)
+            {
+                connection.Open();
+
+                return connection.Query<Test>(
+                    "SELECT * FROM test WHERE browser = @Browser", new { Browser })
+                    .ToList();
+            }
+        }
+    }
+}
