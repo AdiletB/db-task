1ad9e1cfa326840afcbbbc2275197b9eeff9f5f6
AdiletB <a.balabekov@outlook.com>
2024-12-09 19:04:54 +0500

    Add tests

---
+++ DbTask/CRUD_Scenarios/BaseTest.cs
@@ -0,0 +1,22 @@
+﻿using DbTask.DataAccess.Models;
+
+namespace DbTask.Tests.CRUD_Scenarios
+{
+    public class BaseTest
+    {
+        protected List<Entity> Entities { get; } = new List<Entity>();
+        protected long NewAuthorId { get; private set; }
+
+        [OneTimeSetUp]
+        public void CreateAuthor()
+        {
+        
+        }
+
+        [OneTimeTearDown]
+        public void DeleteEntities()
+        {
+
+        }
+    }
+}
---
+++ DbTask/CRUD_Scenarios/Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbTask.DataAccess.Queries.Tests;
+using DbTask.Tests.CRUD_Scenarios;
+
+namespace DbTask.Tests.Scenarios
+{
+    public class Tests : BaseTest
+    {
+        [Test]
+        public void UpdateAuthorForChromeTests()
+        {
+            var chromeTests = new GetByBrowser("Chrome").Execute();
+            Assert.That(chromeTests.Count(), Is.GreaterThan(0));
+        }
+    }
+}
