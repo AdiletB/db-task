64f53e28d503bdf6736dccf6125bbd8964f511ad
AdiletB <a.balabekov@outlook.com>
2024-12-10 12:11:09 +0500

    Implement author deletion by id

---
+++ DbTask.DataAccess/Commands/Authors/RemoveAuthorById.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace DbTask.DataAccess.Commands.Authors
+{
+    public class RemoveAuthorById : ICommand
+    {
+        public long AuthorId { get; set; }
+
+        public RemoveAuthorById(long authorId)
+        {
+            AuthorId = authorId;
+        }
+
+        public long Execute()
+        {
+            using (var conection = Database.Instance.Connection)
+            {
+                conection.Open();
+
+                return conection.Execute("DELETE FROM author WHERE id = @AuthorId", new { AuthorId });
+            }
+        }
+    }
+}
