31682277a277d91e8c3a517822ea34fcecc13c6f
AdiletB <a.balabekov@outlook.com>
2024-12-11 17:48:08 +0500

    Read config file and setup db

---
+++ DbTask/SetupTest.cs
@@ -0,0 +1,15 @@
+﻿using DbTask.DataAccess;
+using DbTask.Tests.Utils;
+
+namespace DbTask.Tests
+{
+    [SetUpFixture]
+    public class SetupTest
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            DbConfig.ConnectionString = Config.Get("connectionString");
+        }
+    }
+}
